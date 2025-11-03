cd9a157bd48d782ba05fa6c6456bc41e53acada9
Stalker002 <148716603+Stalker002@users.noreply.github.com>
2025-11-04 00:07:59 +0300

    feat: Improve NotFoundPage layout

--- react-app/src/pages/NotFoundPage.jsx
+++ react-app/src/pages/NotFoundPage.jsx
@@ -5,13 +5,9 @@ import './NotFoundPage.css'
 function NotFoundPage() {
 
   return (
-    <>
-      <MainHeader />
-      <div className="not-found-page">
-        <h1>404 Not Found Page</h1>
-      </div>
-      <Footer />
-    </>
+    <div className="not-found-page">
+      <h1>404 Not Found Page</h1>
+    </div>
   )
 }
 
