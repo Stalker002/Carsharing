ac27d592ce92bfba9964f29a5e236c27766f8cf4
Stalker002 <148716603+Stalker002@users.noreply.github.com>
2026-04-10 22:13:46 +0300

    fix(data): restore design-time db context compilation

--- Carsharing/Carsharing.DataAccess/CarsharingDbContextFactory.cs
+++ Carsharing/Carsharing.DataAccess/CarsharingDbContextFactory.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Carsharing.DataAccess;
 
