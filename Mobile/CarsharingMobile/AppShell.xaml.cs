777ef3cd2531b0ca232652c4ae134d15f5831e00
Stalker002 <148716603+Stalker002@users.noreply.github.com>
2026-04-10 16:17:31 +0300

    feat(mobile): add flyout shell navigation

--- Mobile/CarsharingMobile/AppShell.xaml.cs
+++ Mobile/CarsharingMobile/AppShell.xaml.cs
@@ -13,6 +13,8 @@ public partial class AppShell : Shell
         Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
         Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
         Routing.RegisterRoute(nameof(CurrentTripPage), typeof(CurrentTripPage));
+        Routing.RegisterRoute(nameof(RegistrationPage), typeof(RegistrationPage));
+        //Routing.RegisterRoute(nameof(CarDetailsPage), typeof(CarDetailsPage));
         //Routing.RegisterRoute(nameof(DocumentsPage), typeof(DocumentsPage));
     }
 }
