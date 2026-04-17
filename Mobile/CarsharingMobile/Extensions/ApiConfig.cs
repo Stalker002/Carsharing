2741f410f7d5de9838c0e598fdd7c6b8d5695679
Stalker002 <148716603+Stalker002@users.noreply.github.com>
2026-04-17 17:31:36 +0300

    refactor(mobile) change ip

--- Mobile/CarsharingMobile/Extensions/ApiConfig.cs
+++ Mobile/CarsharingMobile/Extensions/ApiConfig.cs
@@ -4,7 +4,7 @@ public static class ApiConfig
 {
     public const string Port = "5078";
 
-    public const string LocalIpAddress = "172.23.201.129";
+    public const string LocalIpAddress = "172.20.10.8";
 
     public static string HostIp =>
         DeviceInfo.Platform == DevicePlatform.Android && DeviceInfo.DeviceType == DeviceType.Virtual
